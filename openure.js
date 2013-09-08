6b2eff20a83b1de1710d3e6f6d50380181c861ab
brainfish <brian@mopub.com>
2013-09-07 18:48:32 -0700

    WIP fix stringifying circular objects

--- openure.js
+++ openure.js
@@ -197,6 +197,53 @@ Openure = {
                 }, that), true);
             }, that);
         }, 2000);
+    },
+
+    stringifyOnce: function(obj, replacer, indent){
+        var printedObjects = [];
+        var printedObjectKeys = [];
+
+        function printOnceReplacer(key, value){
+            if (printedObjects.length > 2000) { // browsers will not print more than 20K, I don't see the point to allow 2K.. algorithm will not be fast anyway if we have too many objects
+                return 'object too long';
+            }
+
+            var printedObjIndex = false;
+            printedObjects.forEach(function(obj, index) {
+                if (obj===value) {
+                    printedObjIndex = index;
+                }
+            });
+
+            if (key == ''){ //root element
+                printedObjects.push(obj);
+                printedObjectKeys.push("root");
+                return value;
+            }
+
+            else if(printedObjIndex+"" != "false" && typeof(value)=="object"){
+                if ( printedObjectKeys[printedObjIndex] == "root"){
+                    return "(pointer to root)";
+                }else{
+                    if (value !== null) {
+                        return "(see " + (!!value.constructor ? value.constructor.name.toLowerCase()  : typeof(value)) + " with key " + printedObjectKeys[printedObjIndex] + ")";
+                    } else {
+                        return "null";
+                    }
+                }
+            }else{
+
+                var qualifiedKey = key || "(empty key)";
+                printedObjects.push(value);
+                printedObjectKeys.push(qualifiedKey);
+                if(replacer){
+                    return replacer(key, value);
+                }else{
+                    return value;
+                }
+            }
+        }
+        return JSON.stringify(obj, printOnceReplacer, indent);
     }
 };
 
