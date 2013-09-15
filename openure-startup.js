90f9f430033eaccb7f0168ff687890bdf63224a4
brainfish <brian@mopub.com>
2013-09-15 14:10:10 -0700

    NoConflict and HTTPS experiments

--- openure-startup.js
+++ openure-startup.js
@@ -1,17 +1,19 @@
+//var jqscript = document.createElement('script');
+//jqscript.src = "http://code.jquery.com/jquery-1.8.3.min.js";
+//jqscript.type = "text/javascript";
+//document.body.appendChild(jqscript);
+
+//var jqcscript = document.createElement('script');
+//jqcscript.src = "https://raw.github.com/replit/jq-console/master/jqconsole.min.js";
+//jqcscript.type = "text/javascript";
+//document.body.appendChild(jqcscript);
+
 var script = document.createElement('script');
-script.src = "https://raw.github.com/sosaucily/openure/master/openure.js";
+//script.src = "http://openure.dev/openure.js";
+script.src = "https://raw.github.com/brainfish/openure/master/openure.js";
+//script.src = "https://raw.github.com/sosaucily/openure/master/openure.js";
 script.type = "text/javascript";
-document.head.appendChild(script);
-
-var jqscript = document.createElement('script');
-jqscript.src = "http://code.jquery.com/jquery-1.8.3.min.js";
-jqscript.type = "text/javascript";
-document.head.appendChild(jqscript);
-
-var jqcscript = document.createElement('script');
-jqcscript.src = "https://raw.github.com/replit/jq-console/master/jqconsole.min.js";
-jqcscript.type = "text/javascript";
-document.head.appendChild(jqcscript);
+document.body.appendChild(script);
 
 chrome.extension.sendRequest({method: "getOpenureKeys"}, function (response) {
     if (!response) {
